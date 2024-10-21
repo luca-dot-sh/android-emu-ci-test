2223e7af4a1b0cf4c6229ee7aa6f94e25b8e7dcb
luca <luca>
2024-10-21 22:02:11 +0200

    add delays + reboot to test script

--- main.sh
+++ main.sh
@@ -1,3 +1,10 @@
 adb root
 adb remount
-adb shell touch /system/test
+sleep 3
+adb shell sync
+adb reboot
+sleep 100 # TODO replace with polling for system property boot complete
+adb root
+adb remount
+sleep 3
+touch /system/test_file
